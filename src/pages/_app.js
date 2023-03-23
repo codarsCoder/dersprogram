423cece4eabe384dd81b6c553a68fd71ed5194a6
Nurullah Arslan <codarscoder@gmail.com>
2023-03-24 00:57:18 +0300

    deeneme

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,95 +1,61 @@
-// ** Next Imports
-import Head from 'next/head'
-import { Router } from 'next/router'
+import { useState } from 'react';
 
-// ** Loader Import
-import NProgress from 'nprogress'
+const daysOfWeek = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
 
-// ** Emotion Imports
-import { CacheProvider } from '@emotion/react'
-
-// ** Config Imports
-import themeConfig from 'src/configs/themeConfig'
-
-// ** Component Imports
-import UserLayout from 'src/layouts/UserLayout'
-import ThemeComponent from 'src/@core/theme/ThemeComponent'
-
-// ** Contexts
-import { SettingsConsumer, SettingsProvider } from 'src/@core/context/settingsContext'
-
-// ** Utils Imports
-import { createEmotionCache } from 'src/@core/utils/create-emotion-cache'
-
-// ** React Perfect Scrollbar Style
-import 'react-perfect-scrollbar/dist/css/styles.css'
-
-// ** Global css styles
-import '../../styles/globals.css'
-import { Provider } from 'react-redux'
-import store from '../redux/store'
-import { PersistGate } from 'redux-persist/integration/react'
-import { persistStore } from 'redux-persist'
-import { ToastContainer } from 'react-toastify'
-import 'react-toastify/dist/ReactToastify.css';
-import Auth from 'src/auth/Auth'
-import Preloader from 'src/components/Preloader'
-
-
-
-const clientSideEmotionCache = createEmotionCache()
-let persistor = persistStore(store);
-
-// ** Pace Loader
-if (themeConfig.routingLoader) {
-  Router.events.on('routeChangeStart', () => {
-    NProgress.start()
-  })
-  Router.events.on('routeChangeError', () => {
-    NProgress.done()
-  })
-  Router.events.on('routeChangeComplete', () => {
-    NProgress.done()
-  })
-}
-
-// ** Configure JSS & ClassName
-const App = props => {
-  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props
-
-  // Variables
-  const getLayout = Component.getLayout ?? (page => <UserLayout>{page}</UserLayout>)
-
-
+function Form() {
+  const [formFields, setFormFields] = useState([]);
+  
+  const handleAddField = (day) => {
+    const fieldIndex = formFields.findIndex(field => field.day === day);
+    const newFields = [...formFields];
+    newFields[fieldIndex].selects.push('');
+    newFields[fieldIndex].inputs.push('');
+    setFormFields(newFields);
+  };
+  
+  const handleChange = (dayIndex, fieldIndex, field, value) => {
+    const newFields = [...formFields];
+    newFields[dayIndex][field][fieldIndex] = value;
+    setFormFields(newFields);
+  };
+  
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(formFields);
+  };
+  
+  // Initialize formFields array
+  useState(() => {
+    const initialFields = daysOfWeek.map(day => ({
+      day,
+      selects: [''],
+      inputs: ['']
+    }));
+    setFormFields(initialFields);
+  }, []);
+  
   return (
-    <CacheProvider value={emotionCache}>
-      {/* Persist gate Redux in kalıcı olmasını saglamak icin */}
-      <PersistGate loading={null} persistor={persistor}>
-        <Provider store={store}>
-          <Head>
-            <title>{`${themeConfig.templateName} - whereishalal Admin Template`}</title>
-            <meta
-              name='description'
-              content={`${themeConfig.templateName} – whereishalal Admin Dashboard Template – is the most developer friendly & highly customizable Admin Dashboard Template based on MUI v5.`}
-            />
-            <meta name='keywords' content='whereishalal' />
-            <meta name='viewport' content='initial-scale=1, width=device-width' />
-          </Head>
-          <ToastContainer />
-          <Preloader />
-          <Auth>
-            <SettingsProvider>
-              <SettingsConsumer>
-                {({ settings }) => {
-                  return <ThemeComponent settings={settings}>{getLayout(<Component {...pageProps} />)}</ThemeComponent>
-                }}
-              </SettingsConsumer>
-            </SettingsProvider>
-          </Auth>
-        </Provider>
-      </PersistGate>
-    </CacheProvider >
-  )
+    <form onSubmit={handleSubmit}>
+      {formFields.map((field, dayIndex) => (
+        <div key={field.day}>
+          <label>{field.day}</label>
+          {field.selects.map((select, selectIndex) => (
+            <select key={`select-${dayIndex}-${selectIndex}`} value={select} onChange={(event) => handleChange(dayIndex, selectIndex, 'selects', event.target.value)}>
+              <option value="">Choose an option</option>
+              <option value="Option 1">Option 1</option>
+              <option value="Option 2">Option 2</option>
+              <option value="Option 3">Option 3</option>
+            </select>
+          ))}
+          {field.inputs.map((input, inputIndex) => (
+            <input key={`input-${dayIndex}-${inputIndex}`} type="text" value={input} onChange={(event) => handleChange(dayIndex, inputIndex, 'inputs', event.target.value)} />
+          ))}
+          <button type="button" onClick={() => handleAddField(field.day)}>Add field</button>
+        </div>
+      ))}
+      <button type="submit">Submit</button>
+    </form>
+  );
 }
 
-export default App
+export default Form;
